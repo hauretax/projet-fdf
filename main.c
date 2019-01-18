67155d529ddf40e85855e135b68a1346c4dbae4c
Huggo TRICOTTET <hutricot@e2r8p8.42.fr>
2019-01-18 19:19:05 +0100

    lastpushoftonight

--- main.c
+++ main.c
@@ -6,14 +6,32 @@
 /*   By: hutricot <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/17 12:23:14 by hutricot          #+#    #+#             */
-/*   Updated: 2019/01/18 17:39:45 by hutricot         ###   ########.fr       */
+/*   Updated: 2019/01/18 19:18:16 by hutricot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "h.h"
-# include <sys/types.h>
-# include <sys/stat.h>
-# include <fcntl.h>
+#include "h.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int		**creat_tab(t_list *lst)
+{
+	t_list	*tmp;
+	int		i;
+	
+	i = 0;
+	tmp = lst;
+	while (tmp->next != NULL)
+	{
+		printf("%s",tmp->content);
+		i++;
+		tmp = tmp->next;
+	}
+	printf("%d",i);
+	
+
+}
 
 t_list	*creat_lst(char *str)
 {
@@ -23,26 +41,29 @@ t_list	*creat_lst(char *str)
 
 	line = NULL;
 	fd = open(str, O_RDONLY);
-	ft_get_line(fd, &line)
-	l[0] = ft_lstnew(line, ft_strlen(line));
+	ft_get_line(fd, &line);
+	l[0] = ft_lstnew(line, (ft_strlen(line) + 1));
 	free(line);
 	line = NULL;
 	while(ft_get_line(fd, &line))
 	{
-		l[1] = ft_lstnew(line, ft_strlen(line));
-		ft_lstadd_b(l[0], l[1]);
-		printf("%s",line);
+		l[1] = ft_lstnew(line, (ft_strlen(line) + 1));
+		ft_lstadd_b(&l[0], l[1]);
 		free(line);
 		line = NULL;
 	}
 	close (fd);
+	return(l[0]);
 }
 
 int 		main(int c, char **v)
 {
-	t_list
+	t_list	*lst;
+	int		**tab;
+
 	if (c != 2)
 		return (ft_error(0));
-	creat_lst(v[1]);
+	lst = creat_lst(v[1]);
+	tab = creat_tab(lst);
 	return (0);
 }
