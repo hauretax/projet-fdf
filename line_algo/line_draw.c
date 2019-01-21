3e9e1bbc6acc43404110948344938bcfc7741ea7
Philippe SIM <psim@e3r9p6.42.fr>
2019-01-21 17:27:16 +0100

    updated line algo

--- line_algo/line_draw.c
+++ line_algo/line_draw.c
@@ -6,7 +6,7 @@
 /*   By: psim <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/18 16:49:25 by psim              #+#    #+#             */
-/*   Updated: 2019/01/18 18:24:29 by psim             ###   ########.fr       */
+/*   Updated: 2019/01/21 17:18:42 by psim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,23 @@
 #include "header_dot_h.h"
 #include <unistd.h>
 
-void	ft_line(t_line *l1, void *param)
+int		ft_abs(int nb)
+{
+	if (nb < 0)
+		return -nb;
+	else
+		return nb;
+}
+
+void	right_down(t_line *l1, void *param)
 {
 	t_param *w1;
 
 	w1 = (t_param *)param;
-	l1->dx = l1->x1 - l1->x0;
-	l1->dy = l1->y1 - l1->y0;
 	l1->x = l1->x0;
 	l1->y = l1->y0;
+	l1->dy = ft_abs(l1->dy);
+	l1->dx = ft_abs(l1->dx);
 	l1->p = 2 * l1->dy - l1->dx;
 	while (l1->x < l1->x1)
 	{
@@ -39,4 +47,56 @@ void	ft_line(t_line *l1, void *param)
 		}
 		l1->x = l1->x + 1;
 	}
+}
+
+void    right_up(t_line *l1, void *param)
+{
+	t_param *w1;
+
+	w1 = (t_param *)param;
+	l1->x = l1->x0;
+	l1->y = l1->y0;
+	l1->dy = ft_abs(l1->dy);
+	l1->dx = ft_abs(l1->dx);
+	l1->p = 2 * l1->dy - l1->dx;
+	while (l1->x < l1->x1)
+	{
+		if (l1->p >= 0)
+		{
+			mlx_pixel_put(w1->mlx_ptr, w1->win_ptr, l1->x, l1->y, 0xFFFFFF);
+			l1->y = l1->y - 1;
+			l1->p = l1->p + 2 * l1->dy - 2 * l1->dx;
+		}
+		else
+		{
+			mlx_pixel_put(w1->mlx_ptr, w1->win_ptr, l1->x, l1->y, 0xFFFFFF);
+			l1->p = l1->p + 2 * l1->dy;
+		}
+		l1->x = l1->x + 1;
+	}
+	puts("up");
+}
+
+void	ft_line(t_line *l1, void *param)
+{
+	int swp;
+	l1->dx = l1->x1 - l1->x0;
+	l1->dy = l1->y1 - l1->y0;
+
+	if (l1->dx < 0)
+	{
+		swp = l1->x0;
+		l1->x0 = l1->x1;
+		l1->x1 = swp;
+		swp = l1->y0;
+		l1->y0 = l1->y1;
+		l1->y1 = swp;
+	}
+	l1->dx = l1->x1 - l1->x0;
+	l1->dy = l1->y1 - l1->y0;
+	
+	if (l1->dy >= 0)
+		right_down(l1, param);
+	if (l1->dy < 0)
+		right_up(l1, param);
 }
--- line_algo/main.c
+++ line_algo/main.c
@@ -6,7 +6,7 @@
 /*   By: psim <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/15 17:39:08 by psim              #+#    #+#             */
-/*   Updated: 2019/01/18 18:25:55 by psim             ###   ########.fr       */
+/*   Updated: 2019/01/21 17:14:52 by psim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,17 +34,18 @@ int		main(void)
 {
 	t_param win1[1];
 	t_line		line[1];
-	line->x0 = 10;
-	line->y0 = 200;
-	line->x1 = 200;
-	line->y1 = 250;
+	line->x0 = 200;
+	line->y0 = 180;
+	line->x1 = 10;
+	line->y1 = 200;
 
 	win1->data = "couco\n";
 	win1->mlx_ptr = mlx_init();
 	win1->win_ptr = mlx_new_window(win1->mlx_ptr, 500, 500, "mlx 42");
 	ft_line(line, win1);
-	mlx_pixel_put(win1->mlx_ptr, win1->win_ptr, 10, 200, 0x00FFFF);
-	mlx_pixel_put(win1->mlx_ptr, win1->win_ptr, 200, 250, 0x00FFFF);
+	mlx_pixel_put(win1->mlx_ptr, win1->win_ptr, line->x0, line->y0, 0x00FFFF);
+	mlx_pixel_put(win1->mlx_ptr, win1->win_ptr, line->x1, line->y1, 0x00FFFF);
+	mlx_pixel_put(win1->mlx_ptr, win1->win_ptr, 46, 46,  0x00FFFF);
 	mlx_key_hook(win1->win_ptr, deal_key, win1);
 	mlx_loop(win1->mlx_ptr);
 }
